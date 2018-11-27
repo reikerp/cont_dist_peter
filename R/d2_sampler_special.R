5c54233466526cb70fa08e98f1693e9ec702421f
reikerp <peter.reiker@slu.edu>
2018-11-26 22:33:40 -0600

    fixed a bug in the function d2_sampler_special

--- R/d2_sampler_special.R
+++ R/d2_sampler_special.R
@@ -6,47 +6,47 @@
 #' @description
 #' Sample from a 2D Distribution Given FX(x) and F(y|X = x)
 #' This function samples from a 2D distribution given the marginal pdf of X and the conditional pdf of Y given X = x.
-#' Its output is in the form of a list where a sample from the marginal pdf of X is the first element.
+#' Its output is in the form of a data frame where the elements are x and y samples from the specified 2D distribution.
 #'
 #' @param n is the number of samples to generate.
 #' @param fx is the marginal pdf of X.
 #' @param fyx is the conditional pdf of Y given X = x (it is in the form function(y,x) expression).
 #'
-#' @return my_samples is a list of samples from the desired 2D distribution.
+#' @return my_samples is a data frame of x and y samples from the desired 2D distribution.
 #' @export
 #'
 #' @examples
-#' d2_sampler_special(3, function(x) {exp(-x^2/2)/sqrt(2*pi)}, function(y,x){exp(-y^2/2)/sqrt(2*pi)})
-#' d2_sampler_special(3, function(x) {ifelse(x > 0, exp(-x), 0)}, function(y,x){ifelse(0 < y & y < x, 1/x, 0)})
-#' d2_sampler_special(3, function(x) {ifelse(0 < x & x < 1, 1, 0)}, function(y,x){ifelse(0 < y, x*exp(-y*x), 0)})
+#' d2_sampler_special(10^4, function(x) {exp(-x^2/2)/sqrt(2*pi)}, function(y,x){exp(-y^2/2)/sqrt(2*pi)})
+#' d2_sampler_special(10^4, function(x) {ifelse(x > 0, exp(-x), 0)}, function(y,x){ifelse(0 < y & y < x, 1/x, 0)})
+#' d2_sampler_special(10^4, function(x) {ifelse(0 < x & x < 1, 1, 0)}, function(y,x){ifelse(0 < y, x*exp(-y*x), 0)})
 
 d2_sampler_special <- function(n = 1, fx, fyx) {
   # Determine the values to be used with the rejection_sampling function (first time).
-  a = 0
-  b = 0
+  a = -1
+  b = 1
 
   while(integrate(fx, -Inf, a)$value > 1/10^4) a = a - 1
   while(integrate(fx, b, Inf)$value > 1/10^4) b = b + 1
 
   # Now find an approximate value for C (first time).
-  C = optimize(fx, c(a,b), maximum = T)
+  C = fx(optimize(fx, c(a,b), maximum = T)$maximum)
 
   one_sample = function(fx, fyx, a, b, C){
   # Now use rejection sampling to find a value of x.
-  x = rejection_sampling(1, fx, a, b, C$maximum)
+  x = rejection_sampling(1, fx, a, b, C)
 
   # Repeat the same process to find a sample from the conditional pdf of Y given X = x.
   fyx_given_x = function(y) fyx(y,x)
 
-  j = 0
-  k = 0
+  j = -1
+  k = 1
 
   while(integrate(fyx_given_x, -Inf, j)$value > 1/10^4) j = j - 1
   while(integrate(fyx_given_x, k, Inf)$value > 1/10^4) k = k + 1
 
-  D = optimize(fyx_given_x, c(j,k), maximum = T)
+  D = fyx_given_x(optimize(fyx_given_x, c(j,k), maximum = T)$maximum)
 
-  y = rejection_sampling(1, fyx_given_x, j, k, D$maximum)
+  y = rejection_sampling(1, fyx_given_x, j, k, D)
 
   # Return the desired sample as a vector.
   return (c(x,y))
@@ -57,5 +57,14 @@ d2_sampler_special <- function(n = 1, fx, fyx) {
   for (i in 1:n) {
     my_samples[[i]] = one_sample(fx, fyx, a, b, C)
   }
+
+  for (i in 1:n){
+    if (i == 1) my_samplesx = my_samples[[i]][1]
+    if (i != 1) my_samplesx = c(my_samplesx, my_samples[[i]][1])
+    if (i == 1) my_samplesy = my_samples[[i]][2]
+    if (i != 1) my_samplesy = c(my_samplesy, my_samples[[i]][2])
+    if (i == n) my_samples = data.frame(x = my_samplesx, y = my_samplesy)
+  }
+
   return(my_samples)
 }
